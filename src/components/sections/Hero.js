bad453d4da2bdafba2be3e44a2b2a0e0012b5018
Olivia Worsham <Olivia.Worsham@sckans.edu>
2021-08-24 11:19:44 -0500

    live site

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,18 +61,19 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Olivia's world  <span className="text-color-primary">startups</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                Enjoy my crazy creative life
+              
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
                     Get started
                     </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
+                  <Button tag="a" color="dark" wideMobile href="https://github.com/oliviagolf2020/First-day/settings/pages/">
                     View on Github
                     </Button>
                 </ButtonGroup>
