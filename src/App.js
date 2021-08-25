05bc3282a8a7ef370e2614213e2b3f7561a7e6c3
Olivia Worsham <Olivia.Worsham@sckans.edu>
2021-08-24 21:31:42 -0500

    live site

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
       ref={childRef}
       children={() => (
         <Switch>
-          <AppRoute exact path="/portfolo" component={Home} layout={LayoutDefault} />
+          <AppRoute exact path="/portfolio" component={Home} layout={LayoutDefault} />
         </Switch>
       )} />
   );
